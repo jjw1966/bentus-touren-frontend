0f76632142859f035a8f472a83a0abe1a38c85c0
jjw1966 <joachim.j.wilhelm@gmail.com>
2026-07-08 16:26:24 +0200

    Integrate Navbar into App component
    Added Navbar component to the main application layout.

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,22 @@ import Event from "./pages/Event";
 import Tour from "./pages/Tour";
 import Dashboard from "./pages/Dashboard";
 import "./App.css";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/event/:name" element={<Event />} />
+        <Route path="/tour" element={<Tour />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
 export default function App() {
   return (
     <BrowserRouter>
