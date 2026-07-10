c88ffe599392cc610e763cd8cb7b082c1526dcf5
jjw1966 <joachim.j.wilhelm@gmail.com>
2026-07-10 15:49:56 +0200

    Refactor App component and remove routing
    Removed routing components and styles, and wrapped App in React.StrictMode.

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import "./styles.css";
 
-import "./App.css";
-
-import Navbar from "./components/Navbar";
-
-import Dashboard from "./pages/Dashboard";
-import Events from "./pages/Events";
-import Event from "./pages/Event";
-import Tour from "./pages/Tour";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/event/:name" element={<Event />} />
-        <Route path="/tour" element={<Tour />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
