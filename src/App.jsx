9a6b87611e117cd5db5ad23172d5d15a16274171
jjw1966 <joachim.j.wilhelm@gmail.com>
2026-07-10 16:03:12 +0200

    Refactor App component to use Navbar and update routes
    Replaced header and footer with Navbar component, updated route paths.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Dashboard from "./pages/Dashboard.jsx";
-import Tourstallning from "./pages/Tourstallning.jsx";
-import OmBentus from "./pages/OmBentus.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard";
+import Tour from "./pages/Tour";
+import OmBentus from "./pages/OmBentus";
 
 export default function App() {
   return (
     <Router>
-      <header>
-        <h1>Bentus Touren 2026</h1>
-        <nav>
-          <ul className="menu">
-            <li><Link to="/">Dashboard</Link></li>
-            <li><Link to="/tourstallning">Tourställning</Link></li>
-            <li><Link to="/ombentus">Om Bentus</Link></li>
-          </ul>
-        </nav>
-      </header>
-
+      <Navbar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/tourstallning" element={<Tourstallning />} />
+        <Route path="/tour" element={<Tour />} />
         <Route path="/ombentus" element={<OmBentus />} />
       </Routes>
-
-      <footer>© 2026 Bentus Touren</footer>
     </Router>
   );
 }
