e46ce0f428b2c4321acb1d6bb1a337e2596c6b1d
jjw1966 <joachim.j.wilhelm@gmail.com>
2026-07-10 21:29:28 +0200

    Refactor Navbar links for clarity and consistency

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,11 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-inner">
-        <div className="nav-left">
-          <Link to="/" className="nav-logo">Bentus Touren</Link>
-        </div>
+        <Link to="/" className="nav-logo">Bentus Touren</Link>
 
         <div className="nav-right">
           <Link to="/" className="nav-item">Dashboard</Link>
-          <Link to="/tour" className="nav-item">Tourställning</Link>
+          <Link to="/tour" className="nav-item">Tour</Link>
           <Link to="/ombentus" className="nav-item">Om Bentus</Link>
         </div>
       </div>
