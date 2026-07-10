a6bf3c5e5f2f84a8a02efed36c48b2847a97be51
jjw1966 <joachim.j.wilhelm@gmail.com>
2026-07-10 16:04:05 +0200

    Remove server configuration from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // viktigt för Render
-  server: {
-    port: 5173,
-    open: true
-  }
+  base: "./"
 });
