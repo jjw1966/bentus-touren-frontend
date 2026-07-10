e8455a4c9b3d47d193b8a73705de497d8869279b
jjw1966 <joachim.j.wilhelm@gmail.com>
2026-07-10 22:25:41 +0200

    Change logo image based on color scheme
    Update logo source based on color scheme preference.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,15 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
+  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const logoSrc = prefersDark ? "/bentus-logo-dark.png" : "/bentus-logo-light.png";
+
   return (
     <nav className="navbar">
       <div className="nav-inner">
 
         <div className="nav-left">
-          <img src="/logo.png" alt="logo" className="nav-logo-img" />
+          <img src={logoSrc} alt="Bentus logo" className="nav-logo-img" />
           <Link to="/" className="nav-logo-text">Bentus Touren</Link>
         </div>
 
