79c7847db4b3a004111a504c609ddde74307e11e
jjw1966 <joachim.j.wilhelm@gmail.com>
2026-07-10 15:57:53 +0200

    Add OmBentus component with tour description

---
+++ src/pages/OmBentus.jsx
@@ -0,0 +1,8 @@
+export default function OmBentus() {
+  return (
+    <main>
+      <h2>Om Bentus Touren</h2>
+      <p>Bentus Touren är en vänskaplig golftour med fokus på gemenskap och tävlingsglädje.</p>
+    </main>
+  );
+}
